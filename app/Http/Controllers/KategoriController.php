1afd4ea76a086aee13f5cd41eeba004250af6560
nicnais <felkencx@gmail.com>
2026-03-04 22:33:23 +0700

    Add files via upload

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Kategori;
+
+class KategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $kategori = Kategori::all();
+        return view('kategori.index', compact('kategori'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate(['nama_kategori' => 'required|max:100']);
+        Kategori::create($request->all());
+        return redirect()->route('kategori.index')->with('success', 'Kategori berhasil ditambahkan!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kategori $kategori)
+    {
+        return view('kategori.edit', compact('kategori'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Kategori $kategori)
+    {
+        $request->validate(['nama_kategori' => 'required|max:100']);
+        $kategori->update($request->all());
+        return redirect()->route('kategori.index')->with('success', 'Kategori berhasil diupdate!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kategori $kategori)
+    {
+        $kategori->delete();
+        return redirect()->route('kategori.index')->with('success', 'Kategori berhasil dihapus!');
+    }
+}
